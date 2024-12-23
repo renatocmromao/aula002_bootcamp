9986ce55d2028131ccf66e1c64e498cd75197e44
Renato Romão <renatocmromao@gmail.com>
2024-12-22 21:15:32 -0300

    Primeira aula de python

---
+++ aula001.py
@@ -0,0 +1,9 @@
+# PRINT - Utilizamos para exibir alguma informação na tela.
+print('Hello world!')
+
+# Conseguimos imprimir números também.
+print(3+5)
+
+
+# INPUT - Utilizamos quando queremos capturar alguma informação do usuário. Podemos fazer isso ao mesmo tempo que imprimimos na tela.
+print(input('Digite o seu nome: '))
