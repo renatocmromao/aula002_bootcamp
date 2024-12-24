6b7b5bb5ecaacfde60e9f5541408f4cc80850197
Renato Romão <renatocmromao@gmail.com>
2024-12-23 21:02:07 -0300

    Aula 1 de python - variáveis

---
+++ Exercicio_note/exe_note_02.py
@@ -0,0 +1,10 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+soma = (num1 + num2)
+subtracao = (num1 - num2)
+multiplicacao = (num1 * num2)
+divisao = (num1 / num2)
+
+print(f'A Soma dos números será {soma}.\nA Subtração dos números será {subtracao}.')
+print(f'A Multiplicação dos números será {multiplicacao}.\nA Divisão dos números será {divisao}.')
